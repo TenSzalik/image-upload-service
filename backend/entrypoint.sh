1229c05bbb01508839bf5f70f960f5a8725fe9ba
TenSzalik <gracjanchudziak@gmail.com>
2023-09-29 06:13:22 +0200

    fix: Repair loading db

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,12 +4,21 @@ shift
 PRODUCTION="production"
 DEVELOPMENT="development"
 
+wait_for_db() {
+    wait-for-it.sh -h db -p 5432 -t 30
+    echo "Database is ready!"
+}
+
 if [ "$ENV" = "$PRODUCTION" ]; then
+    wait_for_db
+
     python3 manage.py migrate
     python3 manage.py collectstatic --noinput
 fi
 
 if [ "$ENV" = "$DEVELOPMENT" ]; then
+    wait_for_db
+
     python3 manage.py migrate
     python3 manage.py loaddata initial_data.json
     python3 manage.py loaddata initial_user.json
